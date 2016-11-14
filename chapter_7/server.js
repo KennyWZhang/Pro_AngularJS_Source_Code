fdb7ec40b29b73b30019039fd519d649574c08f3
Kenny.W.Zhang <victorypiter@msn.com>
2016-11-14 20:01:48 +0800

    complete chapter 7

--- chapter_7/server.js
+++ chapter_7/server.js
@@ -14,56 +14,56 @@ app.get('/products', (request, response) => {
       "description": " A boat for one person",
       "name": "product #1",
       "price": 275,
-      "id": "05af70"
+      "id": "05af71"
     },
     {
       "category": "categoryA",
       "description": " A boat for one person",
       "name": "product #2",
       "price": 275,
-      "id": "05af70"
+      "id": "05af72"
     },
     {
       "category": "categoryA",
       "description": " A boat for one person",
       "name": "product #3",
       "price": 275,
-      "id": "05af70"
+      "id": "05af73"
     },
     {
       "category": "categoryB",
       "description": " A boat for one person",
       "name": "product #4",
       "price": 275,
-      "id": "05af70"
+      "id": "05af74"
     },
     {
       "category": "categoryB",
       "description": " A boat for one person",
       "name": "product #5",
       "price": 275,
-      "id": "05af70"
+      "id": "05af75"
     },
     {
       "category": "categoryB",
       "description": " A boat for one person",
       "name": "product #6",
       "price": 275,
-      "id": "05af70"
+      "id": "05af76"
     },
     {
       "category": "categoryB",
       "description": " A boat for one person",
       "name": "product #7",
       "price": 275,
-      "id": "05af70"
+      "id": "05af77"
     },
     {
       "category": "categoryB",
       "description": " A boat for one person",
       "name": "product #8",
       "price": 275,
-      "id": "05af70"
+      "id": "05af78"
     }
   ]);
 });
